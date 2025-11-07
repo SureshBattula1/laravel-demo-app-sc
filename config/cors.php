9a4b654486190ad41c299175361138ca3e97a08e
murali <nmurali.ab@gmail.com>
2025-11-07 18:42:19 +0530

    Worked on the permissions

--- config/cors.php
+++ config/cors.php
@@ -10,13 +10,16 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
 
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
     'allowed_origins' => [
         'http://localhost:4200',
         'http://localhost:63993',
         'http://127.0.0.1:4200',
         'http://127.0.0.1:63993',
+        'https://demo.schools.techtrendstalks.com',
+        'https://testing.techtrendstalks.com',
+        'https://testing.api.techtrendstalks.com'
     ],
 
     'allowed_origins_patterns' => [
@@ -24,11 +27,11 @@ return [
         '/^http:\/\/127\.0\.0\.1:\d+$/',
     ],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'Accept', 'Origin'],
 
-    'exposed_headers' => ['Content-Disposition', 'X-Total-Count', 'X-Page', 'X-Per-Page'],
+    'exposed_headers' => [],
 
-    'max_age' => 86400,
+    'max_age' => 0,
 
     'supports_credentials' => true,
 
